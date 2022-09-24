fef0cf588b822845441558be078fb709f93f739b
samv <mariscal@gmail.com>
2022-09-24 01:27:26 -0500

    NO

--- src/app/screens/seeker/seeker/seeker.component.ts
+++ src/app/screens/seeker/seeker/seeker.component.ts
@@ -14,6 +14,8 @@ export class SeekerComponent implements OnInit {
   public Myform!:FormGroup;
   public url:string = environment.url
   public vermas:boolean = false;
+  public arrayJsonBusqueda:any;
+  public arrayGlobal:any;
 
   constructor(private fb: FormBuilder , private prd: GenericService) { }
 
@@ -30,12 +32,46 @@ export class SeekerComponent implements OnInit {
     })
   }
 
- public onSubmit() {  
-       this.prd.getObject(`${this.url}/PATIENT/dd6fec1b-30e2-4e46-945d-4ec8773d7372/REGISTRY?format=json&token=it4s`).subscribe(resp =>{
-        console.log(resp);
-       })
+  public onSubmit() {  
+    console.log(this.Myform.valid);
+    this.Myform.markAllAsTouched();
+    let nombre = this.Myform.value.nhc;
+  let Suburl = `${this.url}/PATIENT/SEARCH?format=json&token=it4s&firstName=${nombre.trim()}&middleName=&lastName=&gender&brithDate&nationalI`;
+         this.prd.getObject(`${Suburl}`).subscribe((resp:any) =>{
+            this.buildShowjson(resp);
+         })
+  
+    } 
+  
+  
+  
+    public buildShowjson(resp:any){
+      if(resp.status==200){
+        let patient = [];
+        patient= resp.body.patients
+        if(!patient)
+       
 
-  } 
+
+        
+        this.arrayJsonBusqueda = [];
+        patient.forEach((e:any) => {
+          let urlGetFoilio=`${this.url}/PATIENT/${e.patient_id}/REGISTRY?format=json&token=it4s`
+         this.prd.getObject(urlGetFoilio).subscribe((resp2:any)=>{
+         let obj =  {
+            "Nombre": resp2.first_name,
+            "Primer_apellido": resp2.middle_name,
+            "Segundo_apellido": '',
+            "Sexo":'',
+            "Fecha_nacimiento": resp2.birth_date
+         }
+            this.arrayJsonBusqueda.push(resp2.body);
+            console.log("BUSQUEDA",this.arrayJsonBusqueda);
+         })
+        });
+  
+      }
+    }
 
   public get f() {
     return this.Myform.controls;
